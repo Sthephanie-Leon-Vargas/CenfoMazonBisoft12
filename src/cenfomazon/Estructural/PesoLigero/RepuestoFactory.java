68a0315eb8fb51e4f3ab54411a0058aa2548b3e6
Larkorin <michael162003@gmail.com>
2023-07-18 21:44:43 -0600

    Codigo del patron flyweight sin interacción a la bd.

---
+++ src/cenfomazon/Estructural/PesoLigero/RepuestoFactory.java
@@ -0,0 +1,32 @@
+package cenfomazon.Estructural.PesoLigero;
+
+import cenfomazon.Estructural.PesoLigero.Intrinseco.DataRepuestos;
+import cenfomazon.Estructural.PesoLigero.modulo.util;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RepuestoFactory {
+
+    private static Map<String, DataRepuestos> gPoolRepuesto = new HashMap<>();
+
+    public static DataRepuestos obtenerRepuesto(String tipoRepuesto, String categoria, String marcaRepuesto) {
+        DataRepuestos data = null;
+
+        String key = util.generarKey(tipoRepuesto, categoria, marcaRepuesto);
+        if(gPoolRepuesto.containsKey(key)) {
+            data = gPoolRepuesto.get(key);
+            util.imprimirIntegranteMapa(key, data, "Repuesto recuperado");
+            System.out.println("----Data recuperada-----");
+        } else {
+            data = new RepuestoNave(tipoRepuesto, categoria, marcaRepuesto);
+            gPoolRepuesto.put(key, data);
+        }
+
+        return data;
+    }
+
+    public static Map<String, DataRepuestos> getgPoolRepuesto() {
+        return gPoolRepuesto;
+    }
+}
---
+++ src/cenfomazon/Estructural/PesoLigero/RepuestoNave.java
@@ -0,0 +1,13 @@
+package cenfomazon.Estructural.PesoLigero;
+
+import cenfomazon.Estructural.PesoLigero.Intrinseco.DataRepuestos;
+
+public class RepuestoNave extends DataRepuestos {
+    public RepuestoNave(String tipoRepuesto, String categoria, String marcaRepuesto) {
+        super(tipoRepuesto, categoria, marcaRepuesto);
+    }
+    @Override
+    public String MostrarCaracteristicas(String pDatosExtra) {
+        return super.MostrarCaracteristicas(pDatosExtra);
+    }
+}
